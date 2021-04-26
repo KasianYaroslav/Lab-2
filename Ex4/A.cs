0f0e2c9dd61664084f7b8611cbb81dcb4a032e4f
KasianYaroslav <83211581+KasianYaroslav@users.noreply.github.com>
2021-04-26 14:36:11 +0300

    Add files via upload

---
+++ Ex4/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("Создан новый экземпляр");
+        }
+    }
+}
